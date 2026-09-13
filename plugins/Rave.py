840e2180b845df93868ddd924f485f16448b73bb
itsRahulyt <hiitanjiroyt@gmail.com>
2026-09-13 13:46:59 +0530

    Update Rave.py

--- plugins/Rave.py
+++ plugins/Rave.py
@@ -1,1 +1,8 @@
-hii
+from pyrogram import Client, filters
+
+@Client.on_message(filters.command("rave") & filters.private)
+async def rave_test(client, message):
+    await message.reply(
+        "🔥 Rave system is active!\n\n"
+        "Next step: we'll connect the 480p / 720p / 1080p files."
+    )
